e5ff7e70596927283a487214347f9316664dfae4
bigtonito39 <bigtonito@hotmail.com>
2019-10-06 22:08:36 -0400

    Jose-Reinoso

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+const funBusBig = document.querySelector("img");
+
+funBusBig.addEventListener("dblclick", () => {
+
+    if (funBusBig.requestFullScreen) {
+        funBusBig.requestFullScreen();
+    } else if (funBusBig.mozRequestFullScreen) {
+        funBusBig.mozRequestFullScreen();
+    } else if (funBusBig.webkitRequestFullScreen) {
+        funBusBig.webkitRequestFullScreen();
+    } else {
+        false;
+    }
+});
